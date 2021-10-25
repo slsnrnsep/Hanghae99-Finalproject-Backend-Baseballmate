31d74e635499f994a33fefd5b0de1cea740f0fb0
slsnrnsep <gkrwjaeoqkr1@gmail.com>
2021-10-25 14:37:52 +0900

    test
    test

---
+++ src/main/java/com/finalproject/backend/baseballmate/BaseballmateApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.backend.baseballmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseballmateApplication.class, args);
+    }
+
+}
