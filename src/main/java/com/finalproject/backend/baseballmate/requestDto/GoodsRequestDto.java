2570f85592b9117273f26b89d3e39693718ee99d
TaeWoong Kim <78454649+Woong5026@users.noreply.github.com>
2021-10-27 22:05:15 +0900

    [Add] : 굿즈 CRUD

---
+++ src/main/java/com/finalproject/backend/baseballmate/requestDto/GoodsRequestDto.java
@@ -0,0 +1,14 @@
+package com.finalproject.backend.baseballmate.requestDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GoodsRequestDto {
+    private String goodsName;
+    private int goodsPrice;
+    private String goodsContent;
+    private String goodsImg;
+
+}
