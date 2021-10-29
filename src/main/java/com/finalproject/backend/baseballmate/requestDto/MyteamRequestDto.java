dec5ebb8f93135d21ed49c592f466cf8fb15338a
slsnrnsep <gkrwjaeoqkr1@gmail.com>
2021-10-29 21:10:37 +0900

    [Add] : 프론트엔드 로그인 요청사항
    Dto에 myteam 추가 , null값 반환

--- src/main/java/com/finalproject/backend/baseballmate/requestDto/MyteamRequestDto.java
+++ src/main/java/com/finalproject/backend/baseballmate/requestDto/MyteamRequestDto.java
@@ -1,10 +1,12 @@
 package com.finalproject.backend.baseballmate.requestDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MyteamRequestDto {
 
     private String myteam;
---
+++ src/main/java/com/finalproject/backend/baseballmate/service/LoginResponseDto.java
@@ -0,0 +1,16 @@
+package com.finalproject.backend.baseballmate.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String token;
+    private String myteam;
+
+
+}
