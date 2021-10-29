68db37d09d7b23099cbbe30fefbba9c178b6ec1f
slsnrnsep <gkrwjaeoqkr1@gmail.com>
2021-10-29 16:12:02 +0900

    [Fix] : 좋아요 버그 수정 ,리액트 요청사항 적용
    한 유저가 계속 좋아요 올리는 버그 수정, 리액트에서 로그인체크시 내가 좋아요누른 게시글넘버들 조회하게 해달라함

--- src/main/java/com/finalproject/backend/baseballmate/repository/TimeLineLikesRepository.java
+++ src/main/java/com/finalproject/backend/baseballmate/repository/TimeLineLikesRepository.java
@@ -3,8 +3,10 @@ package com.finalproject.backend.baseballmate.repository;
 import com.finalproject.backend.baseballmate.model.TimeLineLikes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TimeLineLikesRepository extends JpaRepository<TimeLineLikes,Long> {
     Optional<TimeLineLikes> findByTimeLineIdAndUserId(Long timeLineId, Long UserId);
+    List<TimeLineLikes> findAllByUserId(Long UserId);
 }
