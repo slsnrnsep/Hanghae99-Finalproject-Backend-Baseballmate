f4831e64f32362dfad91720b1515075e8a7b77f5
TaeWoong Kim <78454649+Woong5026@users.noreply.github.com>
2021-11-08 18:40:21 +0900

    [fix] : 굿즈 코멘트좋아요 수정

--- src/main/java/com/finalproject/backend/baseballmate/controller/GoodsCommentLikesController.java
+++ src/main/java/com/finalproject/backend/baseballmate/controller/GoodsCommentLikesController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequiredArgsConstructor
+@RestController
 public class GoodsCommentLikesController {
-    private GoodsCommentLikesService goodsCommentLikesService;
+
+    private final GoodsCommentLikesService goodsCommentLikesService;
 
     @PostMapping("/goods/{goodsId}/comment/{commentId}/like")
     public String GoodsCommentLikePost(
