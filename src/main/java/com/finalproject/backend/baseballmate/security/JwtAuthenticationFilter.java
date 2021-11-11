484770b384632f74c0726ecaab6d248ba00b844a
slsnrnsep <gkrwjaeoqkr1@gmail.com>
2021-11-11 14:33:26 +0900

    [VER] : 프론트요청사항적용 배포버전

--- src/main/java/com/finalproject/backend/baseballmate/security/JwtAuthenticationFilter.java
+++ src/main/java/com/finalproject/backend/baseballmate/security/JwtAuthenticationFilter.java
@@ -21,8 +21,12 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         String token = jwtTokenProvider.resolveToken((HttpServletRequest) request); // 헤더에서 JWT 를 받아옵니다.
-        System.out.print("서버로 들어온 토큰:");
-        System.out.println(token); // 클라이언트에서 받은 token값을 서버 콘솔에 찍어줌.
+        if(token !=null){
+            // 클라이언트에서 받은 token값을 서버 콘솔에 찍어줌.
+        System.out.print("Server provided token:");
+        System.out.println(token.substring(0,20));
+
+        }
         // 유효한 토큰인지 확인합니다.
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // validateToken의 결과가 True이고
