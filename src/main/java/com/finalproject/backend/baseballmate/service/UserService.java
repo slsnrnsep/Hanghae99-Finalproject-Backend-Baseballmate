33608f10f5ec574fce9692fb5dd3760990c9cf68
slsnrnsep <gkrwjaeoqkr1@gmail.com>
2021-11-20 20:01:48 +0900

    [ver] : 로컬테스트x 서버배포버전o

--- src/main/java/com/finalproject/backend/baseballmate/service/UserService.java
+++ src/main/java/com/finalproject/backend/baseballmate/service/UserService.java
@@ -53,17 +53,19 @@ public class UserService {
 
         password = passwordEncoder.encode(userRequestDto.getPassword());
 
-//        User user = userRepository.findByPhoneNumber(userRequestDto.getPhonenumber()).orElseThrow(
-//                ()-> new IllegalArgumentException("휴대폰에 맞는 유저정보를 찾을 수 없습니다.")
-//        );
-//        user.setUserid(userid);
-//        user.setUsername(username);
-//        user.setPassword(password);
-//        user.setPhoneNumber(userRequestDto.getPhonenumber()+"/"+user.getRanNum());
-//        user.setAddress("전국");
-//        user.setPicture("sample.png");
 
-        User user = new User(userid,username,password, userRequestDto.getPhonenumber());
+        User user = userRepository.findByPhoneNumber(userRequestDto.getPhonenumber()).orElseThrow(
+                ()-> new IllegalArgumentException("휴대폰에 맞는 유저정보를 찾을 수 없습니다.")
+        );
+        user.setUserid(userid);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setPhoneNumber(userRequestDto.getPhonenumber()+"#"+user.getRanNum());
+        user.setAddress("전국");
+        user.setPicture("sample.png");
+
+//        User user = new User(userid,username,password, userRequestDto.getPhonenumber());
+
         userRepository.save(user);
         AlarmRequestDto alarmRequestDto = new AlarmRequestDto();
         String signupAlarm = "안녕하세요" + user.getUsername() + "님! 가입을 환영합니다";
