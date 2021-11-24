6d36e9262d5fe2a14342db2ba3ba75bb70c41cf6
codenamehee <jhyfordev@gmail.com>
2021-11-24 20:36:36 +0900

    pull 전 commit

--- src/main/java/com/finalproject/backend/baseballmate/groupChat/ChatRoomController.java
+++ src/main/java/com/finalproject/backend/baseballmate/groupChat/ChatRoomController.java
@@ -48,12 +48,14 @@ public class ChatRoomController {
         return chatMessageService.getChatMessageByRoomId(roomId, pageable);
     }
 
+    // 직관모임 채팅방 나가기
     @ApiOperation(value = "해당 채팅방 나가기", notes = "해당 채팅방 나가기")
     @DeleteMapping("/chat/quit/{groupId}")
     public void quitChat(@PathVariable Long groupId,@AuthenticationPrincipal UserDetailsImpl userDetails){
         chatRoomService.quitChat(groupId,userDetails);
     }
 
+    // 스야모임 채팅방 나가기
     @DeleteMapping("/chat/screen/quit/{screenId}")
     public void quitChat2(@PathVariable Long screenId,@AuthenticationPrincipal UserDetailsImpl userDetails){
         chatRoomService.quitChat2(screenId,userDetails);
